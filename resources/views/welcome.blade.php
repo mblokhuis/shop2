5753053d3a7f404e5d9f4b2ac3fff9e9a06bbf68
vagrant <vagrant@homestead>
2019-02-06 10:32:29 +0000

    Started with the outline/layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,152 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>David Lynch Shop</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto:300,400,700" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <header>
+            <div class="top-nav container">
+                <div class="logo">David Lynch Shop</div>
+                <ul>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Cart</a></li>
+                </ul>
+            </div> <!-- end top-nav -->
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <div class="hero container">
+                <div class="hero-copy">
+                    <h1>All things David Lynch</h1>
+                    <p>A practical example of using CSS Grid for a typical website layout.</p>
+                    <div class="hero-buttons">
+                        <a href="#" class="button button-white">Button 1</a>
+                        <a href="#" class="button button-white">Button 2</a>
+                    </div>
+                </div> <!-- end hero-copy -->
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="hero-image">
+                    <img src="img/macbook-pro-laravel.png" alt="hero image">
+                </div>
+            </div> <!-- end hero -->
+        </header>
+
+        <div class="featured-section">
+            <div class="container">
+                <h1 class="text-center">Movies</h1>
+
+                <p class="section-description text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid earum fugiat debitis nam, illum vero, maiores odio exercitationem quaerat. Impedit iure fugit veritatis cumque quo provident doloremque est itaque.</p>
+
+                <div class="text-center button-container">
+                    <a href="#" class="button">Featured</a>
+                    <a href="#" class="button">On Sale</a>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+
+                <div class="products text-center">
+                    <div class="product">
+                        <a href="#"><img src="img/MulhollandDrive.jpg" alt="product"></a>
+                        <a href="#"><div class="product-name">Mulholland Drive</div></a>
+                        <div class="product-price">$28</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Losthighway.jpg" alt="product"></a>
+                        <a href="#"><div class="product-name">Lost Highway</div></a>
+                        <div class="product-price">$10</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Blue Velvet.jpg" alt="product"></a>
+                        <a href="#"><div class="product-name">Blue Velvet</div></a>
+                        <div class="product-price">$6</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Wildatheart.jpg" alt="product"></a>
+                        <a href="#"><div class="product-name">Wild at Heart</div></a>
+                        <div class="product-price">$20</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Eraserhead.jpg" alt="product"></a>
+                        <a href="#"><div class="product-name">Eraserhead</div></a>
+                        <div class="product-price">$30</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Theartlife.jpg" alt="product"></a>
+                        <a href="#"><div class="product-name">The Art Life</div></a>
+                        <div class="product-price">$22</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Dune.jpg" alt="product"></a>
+                        <a href="#"><div class="product-name">Dune</div></a>
+                        <div class="product-price">$10</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Twinpeaks.jpg" alt="product"></a>
+                        <a href="#"><div class="product-name">Twin Peaks</div></a>
+                        <div class="product-price">$30</div>
+                    </div>
+                    
+                    </div>
+                </div> <!-- end products -->
+
+                <div class="text-center button-container">
+                    <a href="#" class="button">View more products</a>
                 </div>
-            </div>
-        </div>
+
+            </div> <!-- end container -->
+
+        </div> <!-- end featured-section -->
+
+        <div class="blog-section">
+            <div class="container">
+                <h1 class="text-center">Articles</h1>
+
+                <p class="section-description text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et sed accusantium maxime dolore cum provident itaque ea, a architecto alias quod reiciendis ex ullam id, soluta deleniti eaque neque perferendis.</p>
+
+                <div class="blog-posts">
+                    <div class="blog-post" id="blog1">
+                        <a href="#"><img src="img/blog1.png" alt="blog image"></a>
+                        <a href="#"><h2 class="blog-title">Article 1</h2></a>
+                        <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ullam, ipsa quasi?</div>
+                    </div>
+                    <div class="blog-post" id="blog2">
+                        <a href="#"><img src="img/blog2.png" alt="blog image"></a>
+                        <a href="#"><h2 class="blog-title">Article 2</h2></a>
+                        <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ullam, ipsa quasi?</div>
+                    </div>
+                    <div class="blog-post" id="blog3">
+                        <a href="#"><img src="img/blog3.png" alt="blog image"></a>
+                        <a href="#"><h2 class="blog-title">Article 3</h2></a>
+                        <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ullam, ipsa quasi?</div>
+                    </div>
+                </div> <!-- end blog-posts -->
+            </div> <!-- end container -->
+        </div> <!-- end blog-section -->
+
+        <footer>
+            <div class="footer-content container">
+                <div class="made-with"> <i class="fa fa-heart"></i> </div>
+                <ul>
+                    <li>Follow Me:</li>
+                    <li><a href="#"><i class="fa fa-globe"></i></a></li>
+                    <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                </ul>
+            </div> <!-- end footer-content -->
+        </footer>
+
     </body>
 </html>
